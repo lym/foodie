090a557dae6a808aa218ad92e7346ac8a54854cc
Salym Senyonga <salymsash@gmail.com>
2017-11-02 00:37:57 +0300

    Add login functionality
    Existant users should now be able to log into the system

--- src/login-view.js
+++ src/login-view.js
@@ -1,4 +1,86 @@
 import React from 'react';
+import $ from "jquery";
+import _ from "underscore";
+
+import Session from "./models/session";
+
+class LoginForm extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      email: '',
+      password: '',
+      errorMessage: ''
+    };
+    this.baseURL = 'http://127.0.0.1:5000'
+    this.loginEndpoint = this.baseURL + '/login/';
+
+    // this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  serializeForm(form) {
+    /* Transform funky result from `$.serializeArray()` into easy-to-manipulate
+     * JavaScript object
+     */
+    return _.object(_.map(form.serializeArray(), function (item) {
+        // Convert object to tuple of (name, value)
+        return [item.name, item.value];
+    }));
+  }
+
+  loginSuccess(data) {
+    let session = new Session();
+    session.save(data.token);
+    console.log('Token in local storage: ' + localStorage.apiToken);
+  }
+
+  failure() {
+    console.log('Data submission failed');
+    this.setState({errorMessage: 'Please check email or password!'});
+  }
+
+  submitData(data) {
+    /* Does the actual submission of the data to the server */
+    $.post(this.loginEndpoint, data)
+      .done($.proxy(this.loginSuccess, this))
+      .fail($.proxy(this.failure, this));
+  }
+
+  handleSubmit(event) {
+    event.preventDefault();
+    console.log("Attempting to submit value" + this.state);
+    let formData = this.serializeForm($("form")); //.serializeArray();
+    console.log(formData);
+    this.submitData(formData);
+  }
+
+  render() {
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <div className="form-group has-feedback">
+          <input type="email" className="form-control" placeholder="Email"
+                 name="email" required/>
+          <span className="glyphicon glyphicon-envelope form-control-feedback"></span>
+        </div>
+        <div className="form-group has-feedback">
+          <input type="password" className="form-control"
+                 placeholder="Password" name="password" required
+                  />
+          <span className="glyphicon glyphicon-lock form-control-feedback"></span>
+          <span className="error-block">
+            {this.state.errorMessage}
+          </span>
+        </div>
+        <div className="row">
+          <div className="col-xs-12">
+            <button type="submit" className="btn btn-primary btn-block btn-flat">Sign In</button>
+          </div>
+        </div>
+      </form>
+    );
+  }
+}
 
 class LoginView extends React.Component {
   render() {
@@ -6,37 +88,16 @@ class LoginView extends React.Component {
       <div className="hold-transition login-page">
         <div className="login-box">
           <div className="login-logo">
-            <a href="../../index2.html"><b>Foodie</b></a>
+            <a href="#"><b>Foodie</b></a>
           </div>
 
           <div className="login-box-body">
             <p className="login-box-msg">Sign in</p>
+            <LoginForm />
 
-            <form action="../../index2.html" method="post">
-              <div className="form-group has-feedback">
-                <input type="email" className="form-control" placeholder="Email" />
-                <span className="glyphicon glyphicon-envelope form-control-feedback"></span>
-              </div>
-              <div className="form-group has-feedback">
-                <input type="password" className="form-control" placeholder="Password" />
-                <span className="glyphicon glyphicon-lock form-control-feedback"></span>
-              </div>
-              <div className="row">
-                <div className="col-xs-8">
-                  <div className="checkbox icheck">
-                    <label>
-                      <input type="checkbox" /> Remember Me
-                    </label>
-                  </div>
-                </div>
-                <div className="col-xs-4">
-                  <button type="submit" className="btn btn-primary btn-block btn-flat">Sign In</button>
-                </div>
-              </div>
-            </form>
 
-            <a href="#">Forgot Password?</a><br />
-            <a href="register.html" className="text-center">Register new account</a>
+            <br />
+            <a href="#" className="text-center">Register new account</a>
 
           </div>
         </div>
---
+++ src/models/session.js
@@ -0,0 +1,61 @@
+import $ from "jquery";
+import Backbone from 'backbone';
+
+/* This class declaration looks rather contrived due to the fact that Backbone
+ * does not yet fully support ES6 syntax.
+ */
+class Session extends Backbone.Model.extend({}) {
+  /*
+  defaults: {
+    token: null
+  },
+  */
+  constructor() {
+    super();
+    this.defaults =  {
+      token: null
+    }
+  }
+
+  initialize(options) {
+    this.options = options;
+    $.ajaxPrefilter($.proxy(this._setupAuth, this));
+    this.load();
+  }
+
+  load() {
+    var token = localStorage.apiToken;
+    if (token) {
+        this.set('token', token);
+    }
+  }
+
+  save(token) {
+    this.set('token', token);
+    if (token === null) {
+        localStorage.removeItem('apiToken');
+    }
+    else {
+        localStorage.apiToken = token;
+    }
+  }
+
+  delete() {
+    this.save(null);
+  }
+
+  authenticated() {
+    return this.get('token') != null;
+  }
+
+  _setupAuth(settings, originalOptions, xhr) {
+    if (this.authenticated()) {
+      xhr.setRequestHeader(
+        'Authorization',
+        'Token ' + this.get('token')
+      );
+    }
+  }
+}
+
+export default Session;
