14e26e468c2bbc99ec3009e78d29ba037e1c4841
Salym Senyonga <salymsash@gmail.com>
2018-01-29 21:44:44 +0300

    [Chore] Add recipe show route

--- src/application.js
+++ src/application.js
@@ -6,6 +6,7 @@ import LoginView from './login-view';
 import MaterialUITestView from './material_ui_test_view';
 import RecipesIndex from './recipes_index';
 import NewRecipeView from './new-recipes-view';
+import RecipeShowView from './recipe-show-view';
 
 class Application extends React.Component {
 
@@ -16,6 +17,7 @@ class Application extends React.Component {
         <Route path="/login" component={LoginView} />
         <Route path="/new_recipe" component={NewRecipeView} />
         <Route path="/material_ui_test" component={MaterialUITestView} />
+        <Route path="/recipes/:id" component={RecipeShowView} />
         <Route path="/recipes" component={RecipesIndex} />
       </Switch>
     );
