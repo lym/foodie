16972fc1461b6f0cc7c130c11a6dd6296d7257fc
Salym Senyonga <salymsash@gmail.com>
2018-01-29 21:45:25 +0300

    [Chore] Get rid of obsolete imports

--- src/index.js
+++ src/index.js
@@ -2,15 +2,17 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import { BrowserRouter } from 'react-router-dom';
+// import {Provider} from 'react-redux';
 
 import Application from './application';
 import './index.css';
+// import store from './store';
 
 
 ReactDOM.render((
   <MuiThemeProvider>
     <BrowserRouter>
-    <Application />
+      <Application />
     </BrowserRouter>
   </MuiThemeProvider>),
   document.getElementById('app')
