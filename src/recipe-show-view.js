0707a0829b6a4b445a9ea1dbdcc61fd93746a771
Salym Senyonga <salymsash@gmail.com>
2018-02-12 17:32:19 +0300

    [Feature] Add initial iteration of recipe edit view

--- src/recipe-show-view.js
+++ src/recipe-show-view.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import {Link} from 'react-router-dom';
 import AppBar from 'material-ui/AppBar';
-import {Card, CardHeader, CardMedia, CardText, CardTitle} from 'material-ui/Card';
+import {Card, CardActions, CardHeader, CardMedia, CardText, CardTitle} from 'material-ui/Card';
+import FlatButton from 'material-ui/FlatButton';
 import {GridList, GridTile} from 'material-ui/GridList';
 import {List, ListItem} from 'material-ui/List';
 import Paper from 'material-ui/Paper';
@@ -16,28 +17,39 @@ import _ from "underscore";
 
 import session from "./models/session";
 import Recipes from './collections/recipes';
+import Recipe from './models/recipe.js';
 import Dashboard from './dashboard';
 import MaterialUITestView from './material_ui_test_view';
+import EditRecipeView from './edit-recipe-view';
 
 
 class RecipeShowView extends React.Component {
   constructor(props) {
-    let recipes = new Recipes();
     super(props);
-    console.log('Current props' + this.props);
-    console.log('URL param is: ' + props.match.params.id);  // Access url param
-    console.log('inspecting recipe collection....' + recipes.length);
-    console.log('Inspecting Recipe models....' + recipes.models);
-    console.log('Retrieving model....' + recipes.get(props.match.params.id));
+    let recipes = Recipes;
+    this.recipe = new Recipe(recipes.url + props.match.params.id + '/');
+    this.recipe.fetch({
+      /*
+       * The `success` and `error` callbacks receive three args:
+       * @model
+       * @response:
+       * @options
+      */
+      success: this.setRecipes,
+      error: function (model, response, options) {
+        console.log('Error: Model not retrieved');
+      }
+    })
+    super(props);
     this.state = {
       logged_in: session.authenticated() ? true : false,
-      recipe: {
-        'id': 2,
-        'title': 'Client-side test recipe',
-        'description': 'This test recipe appears only in JavaScript!',
-        'fulfilled': true
-      }
+      editMode: false,
+      recipe: {}
     };
+  }
+
+  setRecipes = (model, response, options) => {
+    this.setState({recipe: response});
   }
 
   renderRecipes() {
@@ -48,69 +60,90 @@ class RecipeShowView extends React.Component {
     window.location.pathname = '/new_recipe';
   }
 
+  editRecipe = (event, recipe) => {
+    console.log('Received recipe ID is: ' + recipe['id']);
+    // this.props.history.push('/recipes/' + this.state.recipe['id'] + '/edit/');
+    this.setState({editMode: true});
+  }
+
   render() {
-    return (
-      <div>
-        <Toggle
-            label="Logged in/out"
-            defaultToggled={true}
-            onToggle={(i, j) => this.handleChange(i, j)}
-            labelPosition="right"
-            style={{margin: 20}}
-        />
-        <AppBar
-          title="Foodie"
-          iconElementRight={this.state.logged_in ? <LoggedIn /> : <LoginButton />}
-        />
+    if (!this.state.editMode) {
+      if (!this.state.recipe) {
+        return <h1>Loading recipes</h1>;
+      }
+      return (
+        <div>
+          <Toggle
+              label="Logged in/out"
+              defaultToggled={true}
+              onToggle={(i, j) => this.handleChange(i, j)}
+              labelPosition="right"
+              style={{margin: 20}}
+          />
+          <AppBar
+            title="Foodie"
+            iconElementRight={this.state.logged_in ? <LoggedIn /> : <LoginButton />}
+          />
 
-        <div className="container-fluid">
-          <div className="row">
-            <div className="col-xs-3 col-md-3">
-              <Paper>
-                <List>
-                  <ListItem
-                    primaryText="New Recipe"
-                    onClick={this.renderNewRecipeForm}
-                  />
-                  <ListItem
-                    primaryText="All Recipes"
-                    onClick={this.renderRecipes}
-                  />
-                </List>
-              </Paper>
-            </div>
-            <div className="col-xs-9 col-md-9">
-              <Paper>
-                <div className="login-box-body">
+          <div className="container-fluid">
+            <div className="row">
+              <div className="col-xs-3 col-md-3">
+                <Paper>
+                  <List>
+                    <ListItem
+                      primaryText="New Recipe"
+                      onClick={this.renderNewRecipeForm}
+                    />
+                    <ListItem
+                      primaryText="All Recipes"
+                      onClick={this.renderRecipes}
+                    />
+                  </List>
+                </Paper>
+              </div>
+              <div className="col-xs-9 col-md-9">
+                <Paper>
+                  <div className="login-box-body">
+                    <Card>
+                      <CardHeader title={this.state.recipe['title']}>
+                      </CardHeader>
+                      {/*
+                      <CardMedia
+                        overlay={<CardTitle title={this.state.recipe.title} subtitle="Overlay subtitle" />}
+                        >
+                        <img
+                          src="http://yummy-recipez.herokuapp.com/static/images/generic_recipe_image.png"
+                          alt= "Generic recipe Image" // height="100" width="100"
+                        />
+                      </CardMedia>
+                      */}
+                      <CardText>
+                        {this.state.recipe['description']}
+                      </CardText>
 
-                  <p>Individual recipe will be here! </p>
-                  <Card>
-                    <CardHeader title={this.state.recipe.title}>
-                    </CardHeader>
-                    {/*
-                    <CardMedia
-                      overlay={<CardTitle title={this.state.recipe.title} subtitle="Overlay subtitle" />}
-                      >
-                      <img
-                        src="http://yummy-recipez.herokuapp.com/static/images/generic_recipe_image.png"
-                        alt= "Generic recipe Image" // height="100" width="100"
-                      />
-                    </CardMedia>
-                    */}
-                    <CardText>
-                      {this.state.recipe.description}
-                    </CardText>
-                  </Card>
-                  <br />
+                      <CardActions>
+                        <FlatButton
+                          // href={'/recipes/' + this.state.recipe['id'] + '/edit/'}
+                          label="Edit Recipe" recipemodel={this.state.recipe}
+                          onClick={(ev, recipe) => this.editRecipe(ev, this.state.recipe)}
+                        />
+                        <FlatButton label="Delete Recipe" />
+                      </CardActions>
+                    </Card>
+                    <br />
 
-                </div>
-              </Paper>
+                  </div>
+                </Paper>
+              </div>
             </div>
           </div>
-        </div>
 
-      </div>
-    );
+        </div>
+      );
+    }
+    else {
+      return (<EditRecipeView recipe={this.state.recipe}/>);
+    }
   }
 }
 
