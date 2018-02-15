8c73aee83a194f0dc49005184005d32920fbbf02
Salym Senyonga <salymsash@gmail.com>
2018-02-15 12:49:16 +0300

    [Chore] Make LoggedIn a class-based component
    This is so we can have consistency across the codebase, as regards
    React component definition.

--- src/logged-in.js
+++ src/logged-in.js
@@ -1,35 +1,37 @@
+import React from 'react';
+import {withRouter} from 'react-router-dom';
 import IconButton from 'material-ui/IconButton';
 import IconMenu from 'material-ui/IconMenu';
 import MenuItem from 'material-ui/MenuItem';
 import MoreVertIcon from 'material-ui/svg-icons/navigation/more-vert';
-import React from 'react';
 
 import session from './models/session';
 
-let logout;
-logout = () => {
-  session.delete();
-  window.location.pathname = 'login';
+
+class LoggedIn extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+  static muiName = 'IconMenu';
+
+  logout = () => {
+    session.delete();
+    this.props.history.push('/login');
+  }
+
+  render () {
+    return (
+      <IconMenu
+        iconButtonElement={<IconButton><MoreVertIcon /></IconButton>}
+        targetOrigin={{horizontal: 'right', vertical: 'top'}}
+        anchorOrigin={{horizontal: 'right', vertical: 'top'}}
+      >
+        <MenuItem primaryText="Help" />
+        <MenuItem primaryText="Settings" />
+        <MenuItem primaryText="Sign Out" onClick={this.logout} />
+      </IconMenu>
+    );
+  }
 }
 
-const LoggedIn = (props) => (
-  <IconMenu
-    {...props}
-    iconButtonElement={
-      <IconButton><MoreVertIcon /></IconButton>
-    }
-    targetOrigin={{horizontal: 'right', vertical: 'top'}}
-    anchorOrigin={{horizontal: 'right', vertical: 'top'}}
-  >
-    <MenuItem primaryText="Help" />
-    <MenuItem primaryText="Settings" />
-    <MenuItem
-      primaryText="Sign Out"
-      onClick={logout}
-    />
-  </IconMenu>
-);
-
-LoggedIn.muiName = 'IconMenu';
-
-export default LoggedIn;
+export default withRouter(LoggedIn);
