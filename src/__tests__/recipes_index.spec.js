90cfdc54eed862bb23a4bfacf475a8d1d9aec427
Salym Senyonga <salymsash@gmail.com>
2018-02-19 12:32:48 +0300

    [Chore] Add more tests for RecipesIndexView

--- src/__tests__/recipes_index.spec.js
+++ src/__tests__/recipes_index.spec.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { expect } from 'chai';
 import {shallow, mount} from 'enzyme';
+import { spy } from 'sinon';
 
 import {withRouter, Router, MemoryRouter} from 'react-router-dom';
 import Paper from 'material-ui/Paper';
@@ -9,6 +10,7 @@ import RecipesIndex from '../recipes_index';
 import {FoodieAppBar} from '../components/foodie-app-bar';
 import {FoodieSidebarMenu} from '../components/foodie-sidebar-menu';
 
+spy(RecipesIndex.prototype, 'componentDidMount');
 
 describe('the environment', () => {
   it('works, hopefully', () => {
@@ -17,11 +19,26 @@ describe('the environment', () => {
 });
 
 describe('RecipesIndex', () => {
+  it('calls componentDidMount', () => {
+    const wrapper = mount(<RecipesIndex />);
+    console.log(wrapper.instance());
+    expect(RecipesIndex.prototype.componentDidMount.calledOnce).to.equal(true);
+  });
+
   it('Should not allow non-logged-in users to access the recipes list', () => {
     const wrapper = shallow(<MemoryRouter><RecipesIndex/></MemoryRouter>);
-    console.log(wrapper.html());
     expect(wrapper.html()).to.contain(
       '<h1>Your session expired, please sign in</h1>'
     );
   });
+
+  it('Should be in logged-out state initially', () => {
+    const wrapper = shallow(<RecipesIndex/>);
+    expect(wrapper.state('logged_in')).to.equal(false);
+  });
+
+  it('should have an empty recipes list', () => {
+    const wrapper = shallow(<RecipesIndex/>);
+    expect(wrapper.state('recipes').length).to.equal(0);
+  })
 });
